fded43d3f953392102fcae0624cdd84cc30d81ba
Lorel <lorel.oboue@gmail.com>
2022-08-23 00:02:20 +0100

    some select queries from standard criteria added

--- sql/select-films.sql
+++ sql/select-films.sql
@@ -1,1 +1,26 @@
 SELECT * FROM films;
+
+SELECT * FROM films ORDER BY score DESC;
+
+SELECT * FROM films ORDER BY release_year ASC;
+
+SELECT * FROM films WHERE score >= 8;
+
+SELECT * FROM films WHERE score <= 7;
+
+SELECT * FROM films WHERE release_year = 1990;
+
+SELECT * FROM films WHERE release_year < 2000;
+
+SELECT * FROM films WHERE release_year BETWEEN 1990 AND 1999;
+
+SELECT * FROM films WHERE genre = 'SciFi';
+
+SELECT * FROM films WHERE genre = 'Western' OR genre = 'SciFi';
+
+SELECT * FROM films WHERE genre != 'SciFi';
+
+
+
+
+
